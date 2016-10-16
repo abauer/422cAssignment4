d1a271cc5bb28acec4a0512c8c3beb5663a24ef4
abauer <ambauer@utexas.edu>
2016-10-16 17:59:12 -0500

    half of stage 1

--- Critter.java
+++ Critter.java
@@ -12,7 +12,13 @@
  */
 package assignment4;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /* see the PDF for descriptions of the methods and fields in this class
  * you may add fields, methods or inner classes to Critter ONLY if you make your additions private
@@ -22,8 +28,8 @@ import java.util.List;
 
 public abstract class Critter {
 	private static String myPackage;
-	private	static List<Critter> population = new java.util.ArrayList<Critter>();
-	private static List<Critter> babies = new java.util.ArrayList<Critter>();
+	private	static List<Critter> population = new ArrayList<Critter>();
+	private static List<Critter> babies = new ArrayList<Critter>();
 
 	// Gets the package name.  This assumes that Critter and its subclasses are all in the same package.
 	static {
@@ -48,19 +54,44 @@ public abstract class Critter {
 	
 	private int x_coord;
 	private int y_coord;
-	
+
+	private final void moveInDirection(int direction, int distance) {
+		// right
+		if (direction == 7 || direction == 0 || direction == 1)
+			x_coord = (x_coord+distance)%Params.world_width;
+		// up
+		if (direction == 1 || direction == 2 || direction == 3)
+			y_coord = (y_coord+distance)%Params.world_height;
+		// down
+		if (direction == 3 || direction == 4 || direction == 5)
+			x_coord = (x_coord+distance)%Params.world_width;
+		// left
+		if (direction == 1 || direction == 2 || direction == 3)
+			y_coord = (y_coord+distance)%Params.world_height;
+	}
+
 	protected final void walk(int direction) {
+		energy -= Params.walk_energy_cost;
+		moveInDirection(direction, 1);
 	}
 	
 	protected final void run(int direction) {
-		
+		energy -= Params.run_energy_cost;
+		moveInDirection(direction, 2);
 	}
 	
 	protected final void reproduce(Critter offspring, int direction) {
+		if (energy < Params.min_reproduce_energy) return;
+		offspring.energy = energy/2;
+		energy -= energy/2;
+		offspring.x_coord = x_coord;
+		offspring.y_coord = y_coord;
+		offspring.moveInDirection(direction, 1);
+		babies.add(offspring);
 	}
 
 	public abstract void doTimeStep();
-	public abstract boolean fight(String oponent);
+	public abstract boolean fight(String opponent);
 	
 	/**
 	 * create and initialize a Critter subclass.
@@ -73,6 +104,11 @@ public abstract class Critter {
 	 * @throws InvalidCritterException
 	 */
 	public static void makeCritter(String critter_class_name) throws InvalidCritterException {
+		try {
+			population.add((Critter)Class.forName(critter_class_name).newInstance());
+		} catch(Exception e){
+			throw new InvalidCritterException("Could not find Critter of type "+critter_class_name);
+		}
 	}
 	
 	/**
@@ -82,8 +118,13 @@ public abstract class Critter {
 	 * @throws InvalidCritterException
 	 */
 	public static List<Critter> getInstances(String critter_class_name) throws InvalidCritterException {
-		List<Critter> result = new java.util.ArrayList<Critter>();
-	
+		List<Critter> result;
+		try {
+			Class c = Class.forName(critter_class_name);
+			result = population.stream().filter(c::isInstance).collect(Collectors.toList());
+		} catch(Exception e){
+			throw new InvalidCritterException("Could not find Critter of type "+critter_class_name);
+		}
 		return result;
 	}
 	
@@ -100,7 +141,7 @@ public abstract class Critter {
 			if (old_count == null) {
 				critter_count.put(crit_string,  1);
 			} else {
-				critter_count.put(crit_string, old_count.intValue() + 1);
+				critter_count.put(crit_string, old_count + 1);
 			}
 		}
 		String prefix = "";
@@ -167,10 +208,22 @@ public abstract class Critter {
 	 * Clear the world of all critters, dead and alive
 	 */
 	public static void clearWorld() {
+		population = new ArrayList<Critter>();
 	}
 	
 	public static void worldTimeStep() {
+		Iterator<Critter> it = population.iterator();
+		while(it.hasNext()){
+			Critter c = it.next();
+			c.doTimeStep();
+		}
 	}
 	
-	public static void displayWorld() {}
+	public static void displayWorld() {
+		String border = "+"
+				+ Collections.nCopies(Params.world_width,"-").stream().collect(Collectors.joining())
+				+ "+";
+		System.out.println(border);
+		System.out.println(border);
+	}
 }
