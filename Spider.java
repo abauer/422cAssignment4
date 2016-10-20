b022816b2237d2e3d794a63ea5a26d7db7e012e6
abauer <ambauer@utexas.edu>
2016-10-20 13:17:26 -0500

    added javadoc for Spider and CowardCritter, added Header to all files

--- Spider.java
+++ Spider.java
@@ -1,7 +1,21 @@
+/* CRITTERS Spider.java
+ * EE422C Project 4 submission by
+ * Anthony Bauer
+ * amb6869
+ * 16480
+ * Grant Uy
+ * gau84
+ * 61480
+ * Slip days used: <0>
+ * Fall 2016
+ */
+
 package assignment4;
 
-/*
- * Example critter
+/**
+ * Spider Critter
+ * The Spider Critter will never move backwards
+ * Upon reproducing the Spider produces two offspring and then kills itself
  */
 public class Spider extends Critter {
 
@@ -11,15 +25,19 @@ public class Spider extends Critter {
     private int dir;
 
     public Spider() {
-        dir = Critter.getRandomInt(8);
+        dir = Critter.getRandomInt(4);
     }
 
-    public boolean fight(String not_used) { run(dir); return true; }
+    public boolean fight(String not_used) {
+        run(dir);
+        dir = Critter.getRandomInt(4);
+        return true;
+    }
 
     @Override
     public void doTimeStep() {
-		/* take one step forward */
         walk(dir);
+        dir = Critter.getRandomInt(4);
         if (getEnergy() > Params.start_energy+1) {
             reproduce(new Spider(), Critter.getRandomInt(8));
             reproduce(new Spider(), Critter.getRandomInt(8));
--- SquareCritter.java
+++ SquareCritter.java
@@ -1,3 +1,14 @@
+/* CRITTERS SquareCritter.java
+ * EE422C Project 4 submission by
+ * Anthony Bauer
+ * amb6869
+ * 16480
+ * Grant Uy
+ * gau84
+ * 61480
+ * Slip days used: <0>
+ * Fall 2016
+ */
 package assignment4;
 
 /*
